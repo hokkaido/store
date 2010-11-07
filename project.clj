00e4dca1dfcf2ee7fe97215818ef66b0b4e9b8ad
Matt Revelle <matt@dynamictyping.org>
2010-11-06 23:45:27 -0400

    redis-clojure is now a dependencny.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [net.java.dev.jets3t/jets3t "0.7.4"]
-		 [clomert "0.2.0"]
-		 [clj-serializer "0.1.0"]]
+                 [redis-clojure "1.2.4"]
+                 [clomert "0.2.0"]
+                 [clj-serializer "0.1.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
-		     [lein-clojars "0.5.0"]])
+                     [lein-clojars "0.5.0"]])
