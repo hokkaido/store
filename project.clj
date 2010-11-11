78559b7fcc637068c5bb519109db130fa8b60ca1
Matt Revelle <matt@dynamictyping.org>
2010-11-11 00:03:26 -0500

    Using latest jedis, deployed to clojars.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [net.java.dev.jets3t/jets3t "0.7.4"]
-                 [redis.clients/jedis "1.3.0"]
+                 [org.clojars.mattrepl/jedis "1.3.2"]
                  [clomert "0.2.0"]
                  [clj-serializer "0.1.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
