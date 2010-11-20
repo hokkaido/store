172525e1e5e7e5ec2aa959ac12ec590947b67dd5
aria42 <me@aria42.com>
2010-11-20 09:24:12 -0800

    Added fs bucket test

--- test/store/core_test.clj
+++ test/store/core_test.clj
@@ -7,3 +7,10 @@
     (s :put "b1" "k1" "v1")
     (is (= (s :get "b1" "k1") "v1"))
     (is (= ["k1"] (s :keys "b1")))))
+
+(deftest asoc-store-test
+  (let [root (java.io.File. ".")
+	s (-> (mk-store) (assoc "fs" (fs-bucket (.getAbsolutePath root))))]
+    (s :put "fs" "my-key" 2)
+    (is (= (s :get "fs" "my-key") 2))
+    (s :delete "fs" "my-key")))
