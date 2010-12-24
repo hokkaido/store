463e2dda8c2ec3c08670f4639e585eb52e123de5
Matt Revelle <mrevelle@gmail.com>
2010-12-24 01:17:10 -0500

    Updated plumbing version.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [net.java.dev.jets3t/jets3t "0.7.4"]
                  [org.clojars.mattrepl/jedis "1.3.2-SNAPSHOT"]
                  [clj-serializer  "0.1.1"]
-                 [clj-sys/plumbing "0.1.2-SNAPSHOT"]
+                 [clj-sys/plumbing "0.1.3-SNAPSHOT"]
                  [ring/ring-core "0.3.1"]
                  [clomert "0.2.0"]
                  [clj-serializer "0.1.0"]]
