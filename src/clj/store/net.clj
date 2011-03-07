bfd7b1ded8d2bb3eae646fd75e82c59d81ac19e9
aria42 <me@aria42.com>
2011-03-06 17:50:41 -0800

    added REST store

--- src/clj/store/net.clj
+++ src/clj/store/net.clj
@@ -4,10 +4,15 @@
         [clojure.contrib.shell :only [sh]]
         [plumbing.serialize :only [reader writer read-str-msg write-str-msg]]
         [plumbing.server :only [client server start]]
-        [plumbing.core :only [with-timeout with-log]]
-	[clojure.string :only [lower-case]])
+        [plumbing.core :only [print-all with-timeout with-log keywordize-map]]
+	[clojure.string :only [lower-case]]
+	[ring.adapter.jetty :only [run-jetty]]
+	[compojure.core :only [GET PUT routes]]
+	[ring.util.codec :only [url-decode url-encode]])
+  (:require   [clj-json.core :as json] [clj-http.client :as client])
   (:import
    (java.net Socket InetAddress)
+   (org.apache.commons.io IOUtils)
    (java.util.concurrent Executors Future Callable TimeUnit)))
 
 (def op-map
@@ -40,12 +45,58 @@
 	       (write-str-msg (writer s) msg))))
    :port port))
 
+(defn rest-bucket-handler [buckets]
+  [(GET "/buckets/:name/:key" {p :params}
+	(println p)
+	(let [b (buckets (url-decode (p :name)))
+	      k (url-decode (p :key))]	    
+	  {:body (json/generate-string (bucket-get b k))
+	   :content-type "application/json"
+	   :status 200}))
+   (PUT "/buckets/:name/:key" {p :params body :body}
+	(let [k (url-decode (p :key))
+	      v (json/parse-string (IOUtils/toString body "UTF8"))
+	      b (buckets (url-decode (p :name)))]
+	  (try (bucket-put b k v)
+	       {:body  (json/generate-string {"status" "ok"})
+		:content-type "application/json"
+		:status 200}
+	       (catch Exception e
+		 {:body (json/generate-string {"error"
+					       ((print-all :ex :stack :args :fn)
+						e bucket-put [k v])})
+		  :status 500
+		  :content-type "application/json"}))))])
+
+(defn start-rest-bucket-server [buckets port]
+  (run-jetty (apply routes (rest-bucket-handler buckets)) {:port port :join false}))
+
+(defn rest-bucket [name host port & [keywordize-map?]]
+  (let [base (format "http://%s:%d/buckets/%s/" host port name)
+	keywordize (if keywordize-map? keywordize-map identity)]
+    (reify
+     store.api.IReadBucket
+     (bucket-get [this k]
+	  (let [resp (client/get (str base (url-encode k)))]
+	    (cond
+	      (= (:status resp) 200)
+	        (-> resp :body json/parse-string keywordize)
+	      :else
+	        (throw (RuntimeException. (str "server error: " (-> resp :body (get "error"))))))))
+
+     store.api.IWriteBucket
+     (bucket-put [this k v]
+       (let [resp  (client/put (str base (url-encode k)) {:body (.getBytes (json/generate-string v) "UTF8")})]
+	 (cond
+	   (= (:status resp) 200) (:body resp)
+	   :else (throw (RuntimeException. (str "server error: " (-> resp :body (get "error")))))))))))
+
+
 (defn net-bucket-client [host port]
      (partial client host port
 	      (comp first read-str-msg reader)
 	      (fn [s msg]
 		(write-str-msg (writer s) msg))))
-
 
 (defn mk-client-exec [c num-threads future-policy]
   (let [pool (Executors/newFixedThreadPool num-threads)]
